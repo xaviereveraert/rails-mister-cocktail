3b62485b0208942c5eb6383d7e9ba29279d60188
Xavier Everaert <xeveraert@gmail.com>
2020-06-12 16:30:44 +0200

    config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-
-const { environment } = require('@rails/webpacker')
-
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
